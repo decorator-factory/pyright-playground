4e3de56ae89ca654a7ac4afd05fddd797f69856d
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-11-28 17:46:02 +0300

    I was using tabs? How ridiculous.

--- rollup.config.js
+++ rollup.config.js
@@ -9,71 +9,71 @@ import css from 'rollup-plugin-css-only';
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+    let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+    function toExit() {
+        if (server) server.kill(0);
+    }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+    return {
+        writeBundle() {
+            if (server) return;
+            server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+                stdio: ['ignore', 'inherit', 'inherit'],
+                shell: true
+            });
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+            process.on('SIGTERM', toExit);
+            process.on('exit', toExit);
+        }
+    };
 }
 
 
 const devPlugins = [
-	serve(),  // Call `npm run start` once the bundle has been generated
-	livereload("public"),  // Watch the `public` directory and refresh the browser
+    serve(),  // Call `npm run start` once the bundle has been generated
+    livereload("public"),  // Watch the `public` directory and refresh the browser
 ];
 
 
 const productionPlugins = [
-	terser(),  // minify the output
+    terser(),  // minify the output
 ];
 
 
 export default {
-	input: 'src/main.ts',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'svelteApp',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
-			compilerOptions: {
-				// enable run-time checks when not in production
-				dev: !production
-			}
-		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+    input: 'src/main.ts',
+    output: {
+        sourcemap: true,
+        format: 'iife',
+        name: 'svelteApp',
+        file: 'public/build/bundle.js'
+    },
+    plugins: [
+        svelte({
+            preprocess: sveltePreprocess({ sourceMap: !production }),
+            compilerOptions: {
+                // enable run-time checks when not in production
+                dev: !production
+            }
+        }),
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css({ output: 'bundle.css' }),
 
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
+        resolve({
+            browser: true,
+            dedupe: ['svelte']
+        }),
 
-		typescript({
-			sourceMap: !production,
-			inlineSources: !production
-		}),
+        typescript({
+            sourceMap: !production,
+            inlineSources: !production
+        }),
 
-		...production ? productionPlugins : devPlugins
-	],
-	watch: {
-		clearScreen: false
-	}
+        ...production ? productionPlugins : devPlugins
+    ],
+    watch: {
+        clearScreen: false
+    }
 };
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	export let name: string;
+    export let name: string;
 </script>
 
 <main>
-	<h1>Hello, {name}!</h1>
+    <h1>Hello, {name}!</h1>
 </main>
 
 <style lang="scss">
 $h1-color: red;
 
 h1 {
-	color: $h1-color;
+    color: $h1-color;
 }
 </style>
