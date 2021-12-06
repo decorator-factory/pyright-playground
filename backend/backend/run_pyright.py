99cd4c2953028d1c7dd3375e8b568977ffc206b3
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2021-12-07 02:50:17 +0300

    Move public function to the top

--- backend/backend/run_pyright.py
+++ backend/backend/run_pyright.py
@@ -14,6 +14,31 @@ from starlette.responses import JSONResponse, Response
 MAX_REQUEST_BODY_BYTES = 10**7
 
 
+async def run_pyright(req: Request) -> Response:
+    """HTTP handler for the /pyright endpoint."""
+    body = await _read_at_most(req, MAX_REQUEST_BODY_BYTES)
+
+    if body is None:
+        return Response()
+
+    try:
+        json_body = json.loads(body)
+    except ValueError:
+        return JSONResponse({"status": "fail", "error": "Expected JSON"})
+
+    if not isinstance(json_body, dict):
+        return JSONResponse({"status": "fail", "error": "Expected an object"})
+
+    code = json_body.get("code")
+
+    if not isinstance(code, str):
+        return JSONResponse({"status": "fail", "error": "Expected a 'code' field with a string"})
+
+    pyright_output = await _run_pyright_on_code("1.1.190", code)
+
+    return JSONResponse(pyright_output)
+
+
 async def _read_at_most(req: Request, limit: int) -> Optional[bytes]:
     chunks: list[bytes] = []
     length_so_far = 0
@@ -50,28 +75,3 @@ async def _run_pyright_on_code(version: str, code: str) -> object:
         )
         stdout, _stderr = await process.communicate()
     return json.loads(stdout)
-
-
-async def run_pyright(req: Request) -> Response:
-    """HTTP handler for the /pyright endpoint."""
-    body = await _read_at_most(req, MAX_REQUEST_BODY_BYTES)
-
-    if body is None:
-        return Response()
-
-    try:
-        json_body = json.loads(body)
-    except ValueError:
-        return JSONResponse({"status": "fail", "error": "Expected JSON"})
-
-    if not isinstance(json_body, dict):
-        return JSONResponse({"status": "fail", "error": "Expected an object"})
-
-    code = json_body.get("code")
-
-    if not isinstance(code, str):
-        return JSONResponse({"status": "fail", "error": "Expected a 'code' field with a string"})
-
-    pyright_output = await _run_pyright_on_code("1.1.190", code)
-
-    return JSONResponse(pyright_output)
